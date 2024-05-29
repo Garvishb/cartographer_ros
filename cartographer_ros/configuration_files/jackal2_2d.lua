ae10cba294c31a58f157c19a5d36416b7c34a1d4
Garvishb <togarvish@gmail.com>
2024-05-29 18:55:27 -0400

    cartographer mapping 2d setup, testing and tuning

--- cartographer_ros/configuration_files/jackal2_2d.lua
+++ cartographer_ros/configuration_files/jackal2_2d.lua
@@ -20,22 +20,25 @@ options = {
   trajectory_builder = TRAJECTORY_BUILDER,
   map_frame = "map",
   tracking_frame = "base_link",
-  published_frame = "base_link", -- The ROS frame ID to use as the child frame for publishing poses. 
-                            -- For example “odom” if an “odom” frame is supplied by a different part of the system.
+  published_frame = "base_link",-- The ROS frame ID to use as the child frame for publishing poses. 
+                                -- For example “odom” if an “odom” frame is supplied by a different part of the system.
   odom_frame = "odom",
   provide_odom_frame = true, -- publishing a tf between odom_frame and published_frame
                              -- setting this to true when published_frame is also odom gives errors.
-  publish_frame_projected_to_2d = false,
+  publish_frame_projected_to_2d = false, -- the published pose data is strictly in 2D (x, y, yaw) if this is set to true.
+                                         -- This prevents potentially unwanted out-of-plane poses in 2D mode that can occur due to the pose extrapolation step
   use_pose_extrapolator = true,
-  use_odometry = false, -- provide odometry topic in launch file
+  use_odometry = true, -- provide odometry topic in launch file
   use_nav_sat = false,
   use_landmarks = false,
-  publish_tracked_pose=true,
+
+  publish_tracked_pose=true, -- publish the pose of the robot in the odom frame
   publish_to_tf = true, -- publish a tf between map_frame and published_frame
-  num_laser_scans = 1, -- provide laser scan topic in launch file
+  
+  num_laser_scans = 1,
   num_multi_echo_laser_scans = 0,
-  num_subdivisions_per_laser_scan = 1,
-  num_point_clouds = 1, -- provide pointcloud topic in launch file
+  num_subdivisions_per_laser_scan = 10, -- For multi echo laser scan
+  num_point_clouds = 1,
   lookup_transform_timeout_sec = 0.2,
   submap_publish_period_sec = 0.3,
   pose_publish_period_sec = 5e-3,
@@ -47,15 +50,10 @@ options = {
   landmarks_sampling_ratio = 1.,
 }
 
-TRAJECTORY_BUILDER_3D.num_accumulated_range_data = 5
-
-MAP_BUILDER.use_trajectory_builder_3d = true
-MAP_BUILDER.num_background_threads = 7
-POSE_GRAPH.optimization_problem.huber_scale = 5e2
+MAP_BUILDER.use_trajectory_builder_2d = true
+TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 5
 POSE_GRAPH.optimize_every_n_nodes = 5
-POSE_GRAPH.constraint_builder.sampling_ratio = 0.03
-POSE_GRAPH.optimization_problem.ceres_solver_options.max_num_iterations = 10
-POSE_GRAPH.constraint_builder.min_score = 0.62
-POSE_GRAPH.constraint_builder.global_localization_min_score = 0.66
-
+TRAJECTORY_BUILDER_2D.use_imu_data = true -- false to disable the use of IMU data
+TRAJECTORY_BUILDER_2D.min_z = 0.01
+-- TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
 return options
